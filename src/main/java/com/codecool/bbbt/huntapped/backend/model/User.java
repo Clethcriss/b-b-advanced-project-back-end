7345517bf47c037eab9d1cd4a967bbe051c89d74
Toth <toth.balazs.roland@gmail.com>
2018-12-10 11:14:40 +0100

    new model for user, role added

--- src/main/java/com/codecool/bbbt/huntapped/backend/model/User.java
+++ src/main/java/com/codecool/bbbt/huntapped/backend/model/User.java
@@ -1,24 +1,63 @@
 package com.codecool.bbbt.huntapped.backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Data
+import javax.persistence.*;
+import java.util.Set;
+
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Table(name = "user")
 public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
 
-    private @Id @GeneratedValue Long id;
-    private String name;
-    private String nickName;
-    private int totalCheckIns;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
