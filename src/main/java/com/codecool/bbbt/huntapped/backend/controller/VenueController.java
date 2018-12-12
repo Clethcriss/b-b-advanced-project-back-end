fe4dda741c803d8b05a5acbb40611f9dda8d5e6b
Clethcriss <balazs.borsi.1986@gmail.com>
2018-12-12 10:53:39 +0100

    Venue search route response done. List of venues returned if venue contains the given name chunk.

--- src/main/java/com/codecool/bbbt/huntapped/backend/controller/VenueController.java
+++ src/main/java/com/codecool/bbbt/huntapped/backend/controller/VenueController.java
@@ -3,12 +3,16 @@ package com.codecool.bbbt.huntapped.backend.controller;
 
 import com.codecool.bbbt.huntapped.backend.model.Venue;
 import com.codecool.bbbt.huntapped.backend.repository.VenueRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/venue")
 public class VenueController {
@@ -19,5 +23,12 @@ public class VenueController {
     @GetMapping("/{id}")
     public Venue getVenueById(@PathVariable("id") Long id) {
         return venueRepository.findVenueById(id);
+    }
+
+    @GetMapping("/search/{value}")
+    public List<Venue> findVenueByGivenNameCunk(@PathVariable("value") String nameChunk) {
+        List<Venue> venues = venueRepository.findByNameContainingIgnoreCase(nameChunk);
+        log.info("Result of the search on venues: " + venues.toString());
+        return venues;
     }
 }
--- src/main/java/com/codecool/bbbt/huntapped/backend/repository/VenueRepository.java
+++ src/main/java/com/codecool/bbbt/huntapped/backend/repository/VenueRepository.java
@@ -3,7 +3,11 @@ package com.codecool.bbbt.huntapped.backend.repository;
 import com.codecool.bbbt.huntapped.backend.model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VenueRepository extends JpaRepository<Venue, Long> {
 
     Venue findVenueById(Long id);
+
+    List<Venue> findByNameContainingIgnoreCase(String nameChunk);
 }
