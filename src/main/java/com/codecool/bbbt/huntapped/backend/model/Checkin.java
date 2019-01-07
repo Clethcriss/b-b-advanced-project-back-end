d2d0cc74e8526dcc68de2e9057bf5d3f0161874b
Toth <toth.balazs.roland@gmail.com>
2019-01-07 14:42:47 +0100

    checkin repo + relations

--- src/main/java/com/codecool/bbbt/huntapped/backend/model/Checkin.java
+++ src/main/java/com/codecool/bbbt/huntapped/backend/model/Checkin.java
@@ -1,4 +1,29 @@
 package com.codecool.bbbt.huntapped.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Checkin {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Double rating;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Beer beer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Venue venue;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    private Users user;
 }
---
+++ src/main/java/com/codecool/bbbt/huntapped/backend/repository/CheckinRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.bbbt.huntapped.backend.repository;
+
+import com.codecool.bbbt.huntapped.backend.model.Checkin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckinRepository extends JpaRepository<Checkin,Long> {
+}
