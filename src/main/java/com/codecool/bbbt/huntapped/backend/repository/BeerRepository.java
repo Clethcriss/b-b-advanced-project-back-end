0e40d36d23b407fa4ac19f38d86b2d7110c086ab
Clethcriss <balazs.borsi.1986@gmail.com>
2019-01-08 15:34:34 +0100

    New route created for the beers by brewery id request.

--- src/main/java/com/codecool/bbbt/huntapped/backend/repository/BeerRepository.java
+++ src/main/java/com/codecool/bbbt/huntapped/backend/repository/BeerRepository.java
@@ -1,7 +1,9 @@
 package com.codecool.bbbt.huntapped.backend.repository;
 
 import com.codecool.bbbt.huntapped.backend.model.Beer;
+import com.codecool.bbbt.huntapped.backend.model.Brewery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,4 +14,6 @@ public interface BeerRepository extends JpaRepository<Beer, Long> {
     Beer getBeerById(Long id);
 
     List<Beer> findByNameContainingIgnoreCase(String nameChunk);
+
+    List<Beer> findByBrewery(Brewery brewery);
 }
